21b764af740fd3a28d58318a55e35ba39a77fe13
toxyy <thrashtek@gmail.com>
2023-12-02 21:53:51 -0500

    Update day_1_part_2.php

--- day_1_part_2.php
+++ day_1_part_2.php
@@ -1006,13 +1006,13 @@ $total = 0;
 $words = [
     'one'    => 1,
     'two'    => 2,
-    'three' => 3,
-    'four'    => 4,
-    'five'    => 5,
+    'three'  => 3,
+    'four'   => 4,
+    'five'   => 5,
     'six'    => 6,
-    'seven'    => 7,
-    'eight'    => 8,
-    'nine'    => 9
+    'seven'  => 7,
+    'eight'  => 8,
+    'nine'   => 9
 ];
 foreach($codes as $code) {
     $digits = [];
